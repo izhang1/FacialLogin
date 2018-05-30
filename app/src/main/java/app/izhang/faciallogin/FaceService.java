70d815498af5b1f86559e1f60dfeaf8db099aec1
Ivan Zhang <ivanzhangemail@gmail.com>
2018-05-29 21:06:41 -0400

    Initial FaceService
    * Added to interface with the Face API

---
+++ app/src/main/java/app/izhang/faciallogin/FaceService.java
@@ -0,0 +1,18 @@
+package app.izhang.faciallogin;
+
+public class FaceService {
+    /**
+     * isValidFace() Detects whether there's a face and only one face in the picture
+     */
+    public void isValidFace(){
+
+    }
+
+    /**
+     * identifyFace() Detects and identifies whether the saved image and current are the same or not
+     */
+    public void identifyFace(){
+
+    }
+
+}
